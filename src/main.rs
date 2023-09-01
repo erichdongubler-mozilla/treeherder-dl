8aca829323313b71d79f32c7918d3bf974eb2e51
Erich Gubler <erichdongubler@gmail.com>
2023-09-01 12:09:39 -0400

    init: we're pulling jobs!

---
+++ src/main.rs
@@ -0,0 +1,174 @@
+use std::process::exit;
+
+use clap::Parser;
+use regex::Regex;
+use serde::Deserialize;
+use serde_json::Value;
+
+#[derive(Debug, Deserialize)]
+struct Revision {
+    meta: RevisionMeta,
+    results: Vec<RevisionResult>,
+}
+#[derive(Debug, Deserialize)]
+struct RevisionMeta {
+    count: u32,
+}
+#[derive(Debug, Deserialize)]
+struct RevisionResult {
+    id: u32,
+}
+
+#[derive(Debug)]
+struct Jobs(Vec<Job>);
+
+impl<'de> serde::de::Deserialize<'de> for Jobs {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::de::Deserializer<'de>,
+    {
+        #[derive(Debug, Deserialize)]
+        struct JobsArena {
+            job_property_names: Vec<String>,
+            results: Vec<Vec<Value>>,
+        }
+
+        let JobsArena {
+            job_property_names,
+            results,
+        } = Deserialize::deserialize(deserializer)?;
+
+        assert!(results
+            .iter()
+            .all(|res| res.len() == job_property_names.len()));
+
+        macro_rules! assemble_jobs_from_properties {
+            (
+                $($prop:ident),*
+                $(,)?
+            ) => {
+                struct PropIndices {
+                    $($prop: usize,)*
+                }
+                let indices = PropIndices {
+                    $(
+                        $prop: job_property_names
+                            .iter()
+                            .position(|name| name == stringify!($prop))
+                            .expect(concat!(
+                                "failed to extract field `",
+                                stringify!($prop),
+                                "`"
+                            )),
+                    )*
+                };
+
+                #[allow(unused_assignments)]
+                let jobs = results.into_iter().map(|result| {
+                    let mut prop_vals = result.into_iter();
+                    let mut iter_idx = 0usize;
+                    $(
+                        let prop_val =
+                            prop_vals.nth(indices.$prop - iter_idx).expect(concat!(
+                                "failed to get field `",
+                                stringify!($prop),
+                                "` from value"
+                            ));
+                        let $prop = ::serde_json::from_value(prop_val).expect(concat!(
+                            "failed to deserialize field `",
+                            stringify!($prop),
+                            "`"
+                        ));
+                        iter_idx = indices.$prop.checked_add(1).unwrap();
+                    )*
+                    Job {
+                        $($prop,)*
+                    }
+                }).collect::<Vec<_>>();
+
+                Ok(Jobs(jobs))
+                // OPT: Optimal way to do this is likely to identify the next `nth` offset
+            };
+        }
+        assemble_jobs_from_properties! {
+            id,
+            job_type_name,
+            platform,
+            result,
+            state,
+            task_id,
+            platform_option,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct Job {
+    id: u32,
+    job_type_name: String,
+    platform: String,
+    result: String,
+    state: String,
+    task_id: String,
+    platform_option: String,
+}
+
+#[derive(Debug, Parser)]
+struct Cli {
+    revision: String,
+    #[clap(long = "job-type-re")]
+    job_type_name_regex: Option<Regex>,
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let Cli {
+        revision,
+        job_type_name_regex,
+    } = Cli::parse();
+
+    let client = reqwest::Client::new();
+
+    let treeherder_host = "https://treeherder.mozilla.org";
+
+    let revision = client
+        .get(format!(
+            "{treeherder_host}/api/project/try/push/?revision={revision}"
+        ))
+        .send()
+        .await
+        .unwrap()
+        .json::<Revision>()
+        .await
+        .unwrap();
+
+    log::info!("fetching for revision(s): {:?}", [&revision]);
+
+    let Revision { meta, mut results } = revision;
+    let RevisionMeta { count } = meta;
+    assert!(results.len() == usize::try_from(count).unwrap());
+    if count > 1 {
+        log::warn!("more than one `result` found for specified push");
+    } else if count == 0 {
+        log::error!("no `results` found; does the push you specified exist?");
+        exit(1);
+    }
+    let RevisionResult { id: push_id } = results.pop().unwrap();
+
+    let Jobs(mut jobs) = client
+        .get(format!("{treeherder_host}/api/jobs/?push_id={push_id}"))
+        .send()
+        .await
+        .unwrap()
+        .json::<Jobs>()
+        .await
+        .unwrap();
+
+    if let Some(job_type_name_regex) = job_type_name_regex {
+        jobs.retain(|job| job_type_name_regex.is_match(&job.job_type_name));
+    }
+
+    dbg!(&jobs);
+}
